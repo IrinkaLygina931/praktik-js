d18f75a137066d468b084a860162429b775191a1
irpolg <irpolg@ukr.net>
2024-03-02 11:31:59 +0200

    task2

--- js/lessons3.js
+++ js/lessons3.js
@@ -12,3 +12,25 @@
 //     console.log(`Привіт ${name}`);
 // }
 // letMeSeeYourName(greet);
+
+
+// АДАЧА 2
+// Напишіть дві функції:
+// makeProduct(name, price, callback) -приймає імя та ціну товару, 
+// а також колбек.Функція створює обєкт товару, добавляє йому унікальний ідентифікатор, 
+// в значення id і викликає колбек, передаючи йому створений обєкт.
+// showProduct(product) - колбек приймає обєкт продукта і логінує його в консоль;
+// makeProduct("Холодильник", 10000, logger);
+
+// function makeProduct(name, price, getProduct) {
+//     const products = {
+//         name, price, id: Date.now()
+//     }
+//     getProduct(products) 
+// }
+
+// function showProduct(tovar) {
+//     console.log(tovar);
+// }
+
+// makeProduct("Холодильник", 10000, showProduct);
