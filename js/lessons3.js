34639effa4d8b6f0e18a1ecb64516d0120ed4052
Serhii01010101 <jobjob2109@gmail.com>
2024-03-02 12:13:19 +0200

    create task

--- js/lessons3.js
+++ js/lessons3.js
@@ -1,6 +1,6 @@
 // ЗАДАЧА 1
 // Напишіть дві функції:
-// letMeSeeYourName(callback) - запитує імя користувача , через prompt і 
+// letMeSeeYourName(callback) - запитує імя користувача , через prompt і
 // викликає колбек функцію (callback)
 // greet(name) - колбек , який приймає імя і логінує в консоль рядок  "Привіт" + імя
 
@@ -13,11 +13,10 @@
 // }
 // letMeSeeYourName(greet);
 
-
 // АДАЧА 2
 // Напишіть дві функції:
-// makeProduct(name, price, callback) -приймає імя та ціну товару, 
-// а також колбек.Функція створює обєкт товару, добавляє йому унікальний ідентифікатор, 
+// makeProduct(name, price, callback) -приймає імя та ціну товару,
+// а також колбек.Функція створює обєкт товару, добавляє йому унікальний ідентифікатор,
 // в значення id і викликає колбек, передаючи йому створений обєкт.
 // showProduct(product) - колбек приймає обєкт продукта і логінує його в консоль;
 // makeProduct("Холодильник", 10000, logger);
@@ -26,7 +25,7 @@
 //     const products = {
 //         name, price, id: Date.now()
 //     }
-//     getProduct(products) 
+//     getProduct(products)
 // }
 
 // function showProduct(tovar) {
@@ -34,3 +33,105 @@
 // }
 
 // makeProduct("Холодильник", 10000, showProduct);
+
+const scientists = [
+  {
+    name: "Albert",
+    surname: "Einstein",
+    born: 1879,
+    dead: 1955,
+    id: 1,
+  },
+  {
+    name: "Isaac",
+    surname: "Newton",
+    born: 1643,
+    dead: 1727,
+    id: 2,
+  },
+  {
+    name: "Galileo",
+    surname: "Galilei",
+    born: 1564,
+    dead: 1642,
+    id: 3,
+  },
+  {
+    name: "Marie",
+    surname: "Curie",
+    born: 1867,
+    dead: 1934,
+    id: 4,
+  },
+  {
+    name: "Johannes",
+    surname: "Kepler",
+    born: 1571,
+    dead: 1630,
+    id: 5,
+  },
+  {
+    name: "Nicolaus",
+    surname: "Copernicus",
+    born: 1473,
+    dead: 1543,
+    id: 6,
+  },
+  {
+    name: "Max",
+    surname: "Planck",
+    born: 1858,
+    dead: 1947,
+    id: 7,
+  },
+  {
+    name: "Katherine",
+    surname: "Blodgett",
+    born: 1898,
+    dead: 1979,
+    id: 8,
+  },
+  {
+    name: "Ada",
+    surname: "Lovelace",
+    born: 1815,
+    dead: 1852,
+    id: 9,
+  },
+  {
+    name: "Lise",
+    surname: "Meitner",
+    born: 1878,
+    dead: 1968,
+    id: 11,
+  },
+  {
+    name: "Sarah E.",
+    surname: "Goode",
+    born: 1855,
+    dead: 1905,
+    id: 10,
+  },
+  {
+    name: "Hanna",
+    surname: "Hammarström",
+    born: 1829,
+    dead: 1909,
+    id: 12,
+  },
+];
+
+// 1)получити масив вчених які народились у 19ст;
+// 2)порахувати загальну суму років скільки прожили вчені, і визначити середнє значення;
+
+// const wasBorn = scientists
+//   .filter(arr => arr.born > 1800 && arr.born < 1900)
+//   .map(arr => `${arr.name} ${arr.surname}`);
+
+// console.log(wasBorn);
+
+const middleYear = scientists.reduce(
+  (arr, number) => arr + (number.dead - number.born),
+  0
+);
+console.log(middleYear / scientists.length);
